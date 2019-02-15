9aa47f71c8bdc3a8c66783f545c67781dd9648c4
mangAI <2385914340@qq.com>
2019-02-15 00:08:27 -0500

    testing

---
+++ prototype2_test/Assets/script/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MovePlayer();
+	}
+
+    void MovePlayer()
+    {
+        Vector3 player_Position = this.transform.position;
+        player_Position.x=Mathf.RoundToInt(player_Position.x);
+        player_Position.y=Mathf.RoundToInt(player_Position.y);
+        player_Position.z=Mathf.RoundToInt(player_Position.z);
+        if (Input.GetKeyDown(KeyCode.UpArrow)){
+            if (player_Position.x <= -1.9f)
+            {
+                this.transform.Translate(-1, -1, 0);
+                GameObject.FindWithTag("MainCamera").GetComponent<SceneManager>().SetRotateFlag(SceneManager.RotateDirection.up);
+            }
+            else
+                this.transform.Translate(-1, 0, 0);
+        }else if(Input.GetKeyDown(KeyCode.DownArrow)){
+            if (player_Position.x >=1.9f)
+            {
+                this.transform.Translate(1, -1, 0);
+                GameObject.FindWithTag("MainCamera").GetComponent<SceneManager>().SetRotateFlag(SceneManager.RotateDirection.down);
+            }
+            else
+                this.transform.Translate(1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow)){
+            if (player_Position.z <= -1.9f)
+            {
+                this.transform.Translate(0, -1, -1);
+                GameObject.FindWithTag("MainCamera").GetComponent<SceneManager>().SetRotateFlag(SceneManager.RotateDirection.left);
+            }
+            else
+                this.transform.Translate(0, 0, -1);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow)){
+            if (player_Position.z >= 1.9f)
+            {
+                this.transform.Translate(0, -1, 1);
+                GameObject.FindWithTag("MainCamera").GetComponent<SceneManager>().SetRotateFlag(SceneManager.RotateDirection.right);
+            }
+            else
+            {
+                this.transform.Translate(0, 0, 1);
+            }
+        }
+
+    }
+
+   
+
+
+}
---
+++ prototype2_test/Assets/script/SceneManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+    public GameObject GameScene;
+    public GameObject player;
+    bool rotateFlag = false;
+
+    public enum RotateDirection { left,right,up,down};
+
+    float timeCount=0;    //[0-1]
+    public float rotateSpeed;
+
+    Quaternion from;
+    Quaternion to;
+    Quaternion from_player;
+    Quaternion to_player;
+    //float time
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (rotateFlag)
+        {
+            //print(rotateFlag);
+            RotateScene();
+        }
+        
+	}
+
+
+    public void RotateScene(){
+        GameScene.transform.rotation = Quaternion.Slerp(from, to, timeCount);
+       // player.transform.rotation = Quaternion.Slerp(from_player, to_player, timeCount);
+        if (timeCount >= 1)
+        {
+            player.transform.rotation = Quaternion.identity;
+            rotateFlag = false;
+        }
+        timeCount = timeCount + rotateSpeed;
+        
+    }
+
+    public void SetRotateFlag(RotateDirection direction)
+    {
+        timeCount = 0;
+        rotateFlag = true;
+        from = GameScene.transform.rotation;
+        if(direction==RotateDirection.right)
+             to = Quaternion.AngleAxis(-90, Vector3.right) * from;
+        else if (direction == RotateDirection.left)
+            to = Quaternion.AngleAxis(90, Vector3.right) * from;
+        else if(direction == RotateDirection.up)
+            to = Quaternion.AngleAxis(-90, Vector3.forward) * from;
+        else if (direction == RotateDirection.down)
+            to = Quaternion.AngleAxis(90, Vector3.forward) * from;
+        // from_player = player.transform.rotation;
+
+
+
+    }
+}
